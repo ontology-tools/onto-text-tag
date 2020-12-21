637283ff347cf79a441bad3e6593c964507f8a1d
Janna Hastings <janna.hastings@babraham.ac.uk>
2020-12-21 16:52:53 +0100

    Add tagging implementation

--- app.py
+++ app.py
@@ -12,18 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask, url_for
+from flask import Flask, request
 from flask.templating import render_template
-import os
-from config import *
-app = Flask(__name__)
+from ontotagtext import ExtractorComponent
+import spacy
 
+app = Flask(__name__)
 app.config.from_object('config')
-    # Pages for the app
+
+# python -m spacy download en_core_web_md
+# or: en_core_web_sm or en_core_web_lg
+nlp = spacy.load('en_core_web_md')
+
+onto_extractor = ExtractorComponent(
+    nlp,
+    name="ADDICTO",
+    label="ADDICTO",
+    ontologyfile="static/addicto.obo")
+nlp.add_pipe(onto_extractor, after="ner")
+
+
+# Pages for the app
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+# Text tagging app
+@app.route('/tag', methods = ['POST'])
+def tag():
+    text = request.form['inputText']
+    print(f"Got input text {text}")
+    # process the text
+    doc = nlp(text)
+    tag_results = []
+    # get ontology IDs identified
+    for token in doc:
+        if token._.is_ontol_term:
+            print(token._.ontol_id, token.text, token.idx)
+            tag_results.append({"ontol_id":token._.ontol_id,
+                                "span_text":token.text,
+                                "match_index":token.idx})
+    print(f"Got tag results {tag_results}")
+
+    return render_template('index.html',
+                           text=text,
+                           tag_results=tag_results)
+
+
+
 
 if __name__ == "__main__":
     app.run()
