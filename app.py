6adddd86a8f1fa498f28292da059326809737713
Tom <tomjuggler@gmail.com>
2021-01-02 21:52:35 +0200

    added form to enter pubmed ID retrieved form input on server
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- # Copyright 2018 Google LLC
+# Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,45 +38,59 @@ nlp.add_pipe(onto_extractor, after="ner")
 def home():
     return render_template('index.html')
 
+
+@app.route('/pubmed', methods=['POST'])
+def pubmed():
+    id = request.form.get('pubmed_id')
+    # id = request.get_json()
+    print(f"Pubmed id {id}")
+    if id:
+        print(f"Got it {id}")
+        return render_template('index.html')
+    else:
+        print(f"Got nothing")
+        return render_template('index.html')
+    #     text = id)
+    #    tag_results=tag_results)
+
+
 # Text tagging app
-@app.route('/tag', methods = ['POST'])
+
+@ app.route('/tag', methods=['POST'])
 def tag():
-    text = request.form['inputText']
+    text=request.form['inputText']
     print(f"Got input text {text}")
     # process the text
-    doc = nlp(text)
-    tag_results = []
+    doc=nlp(text)
+    tag_results=[]
     # get ontology IDs identified
     for token in doc:
         if token._.is_ontol_term:
-            #print(token._.ontol_id, token.text, token.idx)
-            term = onto_extractor.get_term(token._.ontol_id)
+            # print(token._.ontol_id, token.text, token.idx)
+            term=onto_extractor.get_term(token._.ontol_id)
             if term:
-                ontol_label = term.name
-                ontol_def = str(term.definition)
-                ontol_namespace = term.namespace
+                ontol_label=term.name
+                ontol_def=str(term.definition)
+                ontol_namespace=term.namespace
                 if ontol_namespace is None:
-                    ontol_namespace = term.id[0:term.id.index(":")]
+                    ontol_namespace=term.id[0:term.id.index(":")]
             else:
-                ontol_label = ""
-                ontol_def = ""
-                ontol_namespace = ""
-            tag_results.append({"ontol_id":token._.ontol_id,
-                                "span_text":token.text,
+                ontol_label=""
+                ontol_def=""
+                ontol_namespace=""
+            tag_results.append({"ontol_id": token._.ontol_id,
+                                "span_text": token.text,
                                 "ontol_label": ontol_label,
                                 "ontol_def": ontol_def,
                                 "ontol_namespace": ontol_namespace,
                                 "ontol_link": "http://addictovocab.org/"+token._.ontol_id,
-                                "match_index":token.idx})
+                                "match_index": token.idx})
     print(f"Got tag results {tag_results}")
 
     return render_template('index.html',
-                           text=text,
-                           tag_results=tag_results)
-
-
+                           text = text,
+                           tag_results = tag_results)
 
 
 if __name__ == "__main__":
     app.run()
-    
