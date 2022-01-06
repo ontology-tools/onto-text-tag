bd65fb03ddb6c3a742da32f0d9becb7ab9643992
Tom <tomjuggler@gmail.com>
2022-01-06 20:35:36 +0200

    got title and authors list
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- openalex.py
+++ openalex.py
@@ -1,71 +1,69 @@
 
 
 import requests
-from urllib.request import urlopen
 import json
 
 
 
 def get_one_with_id(id):
     location = f"https://api.openalex.org/works/" + str(id)+ ","    
+    #header is for the "polite pool": https://docs.openalex.org/api#the-polite-pool
     headers = {
         'User-Agent': 'Python-requests/2.25.1',
         'mailto': 'tom@devsoft.co.za'  
     }
     response = requests.get(location, headers=headers)
     alexjson = response.json() if response and response.status_code == 200 else None
-    print(alexjson)
-    
+    # print(alexjson)
+    if alexjson:
+        #title:
+        if 'display_name' in alexjson: 
+            print("title: ", alexjson['display_name'])
+
+        #authors:
+        all_authors = []
+        if 'authorships' in alexjson:
+            for auth in alexjson['authorships']:
+                if 'author' in auth:
+                    if 'display_name' in auth['author']:
+                        all_authors.append(auth['author']['display_name'])
+        print("all_authors: ", all_authors)
+        
+        #abstracts:
+        if 'abstract_inverted_index' in alexjson:                 
+            if alexjson['abstract_inverted_index'] == None:
+                pass
+            else:
+                print("abstract_inverted_index is: ", alexjson['abstract_inverted_index'])
+                return "got an abstract_inverted_index finally!"
+
 
 def get_publications(page, end, alex_id_list):
-    counter = 0
-    #exit recursion:
     print("page: ", page)
+    #exit recursion:
     if page > end:
-        # print("ales_id_list is: ", alex_id_list)
         return (alex_id_list)
     
     #todo: per-page doesn't seem to do anything? per_page also same... default 25 per page
     location = f"https://api.openalex.org/works?page=" + str(page) + "?per-page=50?filter=publication_year:2021"
+    headers = {
+        'User-Agent': 'Python-requests/2.25.1',
+        'mailto': 'tom@devsoft.co.za'  
+    }
     # print("Fetching api json data from query: ", location)
     
-    response = requests.get(location)
+    response = requests.get(location, headers=headers)
     alexjson = response.json() if response and response.status_code == 200 else None
     
     if alexjson:
-        # alex_meta = alexjson['meta']
-        # print("meta: ", alex_meta)
-        # print("Received", alex_meta['count'], "publications")        
-
-        #test:
-        # print("alexjson is: ", alexjson)
-        # print("alexjson type is: ", type(alexjson))
-        # print("alexjson keys are: ", alexjson.keys())
-        # print("datajson values are: ", alexjson.values())
-        # print("datajson items are: ", alexjson.items())
-        
         for result in alexjson['results']:
-            counter = counter + 1
             if 'ids' in result:
-                # print("ids: ", result['ids'])
                 if 'openalex' in result['ids']:
-                    print("openalex: ", result['ids']['openalex'])
                     alex_id_list.append(result['ids']['openalex'])
-
-            # print("checking result: ", counter)
-            # print("result: ", result['id'])
-            # print("name: ", result['display_name'])
-            if 'abstract_inverted_index' in result:                 
-                if result['abstract_inverted_index'] == None:
-                    continue
-                else:
-                    print("abstract_inverted_index is: ", result['abstract_inverted_index'])
-                    return "got one finally!" #none from this type of request, todo: remove this check
         #recurse: 
         page = page+1
         return_alex_id_list = get_publications(page, end, alex_id_list)
         return return_alex_id_list
-
         
     else:
         print("no response?")
@@ -78,12 +76,11 @@ alex_id_url_list = get_publications(1, 1, [])
 #get alex after last "/" in url:
 for alex in alex_id_url_list:
     a = alex.rsplit("/")
-    print("a: ", a[-1])
+    # print("a: ", a[-1])
     alex_id_list.append(a[-1])
 print("done")
 
-
-one = get_one_with_id(alex_id_list[0])
+one = get_one_with_id(alex_id_list[0]) #test only one
 print("got one: ", one)
 
 # one = get_one_with_id("W2741809807")
