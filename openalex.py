7b8eee88c7d30c6bf3859f0029c83d2b35b9cce4
Tom <tomjuggler@gmail.com>
2022-01-06 17:03:13 +0200

    first 20 pages, check for abstract_inverted_index - none found
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- openalex.py
+++ openalex.py
@@ -4,38 +4,44 @@ import requests
 from urllib.request import urlopen
 import json
 
-def get_publications(page):
 
+def get_publications(page):
+    counter = 0
     #exit recursion:
     print("page: ", page)
-    if page > 1: 
+    if page > 20: 
         return page
-
+    
+    #todo: per-page doesn't seem to do anything? per_page also same... default 25 per page
     location = f"https://api.openalex.org/works?page=" + str(page) + "?per-page=50?filter=publication_year:2021"
-    print("Fetching release file from", location)
-    # data = urlopen(location).read()  # bytes
-    # datajson = json.load(data)
+    # print("Fetching api json data from query: ", location)
+    
     response = requests.get(location)
     alexjson = response.json() if response and response.status_code == 200 else None
     
     if alexjson:
         alex_meta = alexjson['meta']
-        print("Received", alex_meta['count'], "publications")
+        print("meta: ", alex_meta)
+        # print("Received", alex_meta['count'], "publications")        
+
+        #test:
         # print("alexjson is: ", alexjson)
         # print("alexjson type is: ", type(alexjson))
         # print("alexjson keys are: ", alexjson.keys())
-
-        #test:
         # print("datajson values are: ", alexjson.values())
         # print("datajson items are: ", alexjson.items())
         
         for result in alexjson['results']:
-            print("result: ", result['id'])
+            counter = counter + 1
+            print("checking result: ", counter)
+            # print("result: ", result['id'])
+            # print("name: ", result['display_name'])
             if 'abstract_inverted_index' in result:                 
                 if result['abstract_inverted_index'] == None:
                     continue
                 else:
                     print("abstract_inverted_index is: ", result['abstract_inverted_index'])
+                    return "got one finally!"
         #recurse: 
         page = page+1
         get_publications(page)
