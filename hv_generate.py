ef02ae9cadf3d3c21638d12ea92e60e7c7ebec11
Tom <tomjuggler@gmail.com>
2022-01-18 11:16:22 +0200

    memory profiler and timeit included read_csv engine="c" works to speed up loading
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- hv_generate.py
+++ hv_generate.py
@@ -13,20 +13,37 @@ import pyhornedowl
 import requests
 from urllib.request import urlopen
 import json
+from memory_profiler import profile
+from timeit import default_timer as timer
 
+import io
 
+#todo: memory usage testing: changing to category
+
+@profile
 def hv_generator(ontology_id_input, should_get_descendents):
+    start_time = timer()
     from app import get_all_descendents #todo: refactor to avoid this circular import
-    df2 = pd.read_csv("static/ontotermmentions.csv",index_col=0)    
-    # print("ontology_id_input type is: ", type(ontology_id_input))
-    # print("received ontology_id_input values: ", ontology_id_input)
-    # ontology_id_list = ontology_id_input 
+    # load csv:
+    df2 = pd.read_csv("static/ontotermmentions.csv", delimiter = ",", index_col=0, engine='c')
+    
+    # load csv with dtype specified (not working): 
+    # df2 = pd.read_csv("static/ontotermmentions.csv", index_col=0, dtype={'ADDICTOID': 'category' ,'LABEL': 'category', 'PMID': 'int64'}) 
+    print("read csv into dataframe")
+    # change dtype to category on the fly (not working):
+    # df2["LABEL"] = df2["LABEL"].astype("category")
+    # df2["ADDICTOID"] = df2["ADDICTOID"].astype("category")   
+
+    print("dtypes: ", df2.dtypes)
 
     # get descendants?
     if should_get_descendents == True:
         ontology_id_list = get_all_descendents(ontology_id_input)
     else:
         ontology_id_list = ontology_id_input 
+    #test: 
+    # ontology_id_list =  ['ADDICTO:0000386', 'ADDICTO:0000803', 'ADDICTO:0000828', 'ADDICTO:0000678']
+    # ontology_id_list = ['ADDICTO:0000311', 'ADDICTO:0000303', 'ADDICTO:0000139', 'ADDICTO:0000138', 'ADDICTO:0000649', 'BCIO:037000', 'ADDICTO:0000941', 'ADDICTO:0000410', 'MF:0000016', 'ADDICTO:0000311', 'ADDICTO:0000303', 'ADDICTO:0000523', 'BCIO:037000', 'ADDICTO:0000352', 'ADDICTO:0000523', 'ADDICTO:0000941', 'ADDICTO:0000410', 'MF:0000016', 'ADDICTO:0000311', 'ADDICTO:0000303', 'ADDICTO:0000311', 'ADDICTO:0000303', 'ADDICTO:0000349', 'IAO:0000088', 'ADDICTO:0000941', 'ADDICTO:0000410', 'MF:0000016', 'ADDICTO:0000311', 'ADDICTO:0000303', 'ADDICTO:0000405', 'ADDICTO:0000836', 'ADDICTO:0000370', 'ADDICTO:0000773', 'ADDICTO:0000405', 'ADDICTO:0000788', 'BCIO:037000', 'ADDICTO:0000139', 'ADDICTO:0000649', 'ADDICTO:0000697', 'BCIO:037000', 'ADDICTO:0000149', 'IAO:0000027', 'BCIO:037000', 'BCIO:037000', 'ADDICTO:0000149', 'ADDICTO:0000941', 'ADDICTO:0000410', 'MF:0000016', 'BCIO:037000', 'BCIO:037000', 'BCIO:037000', 'BCIO:037000', 'CHEBI:18723', 'ADDICTO:0000263', 'CHEBI:3219', 'BCIO:037000', 'ADDICTO:0000147', 'BCIO:037000', 'BCIO:037000', 'ADDICTO:0000147', 'CHEBI:18723', 'ADDICTO:0000262', 'CHEBI:3219', 'http://purl.obolibrary.org/obo/OAE_0000001', 'ADDICTO:0000500', 'http://purl.obolibrary.org/obo/OAE_0000001', 'BCIO:037000', 'BCIO:037000', 'ADDICTO:0000139', 'ADDICTO:0000649', 'ADDICTO:0000697', 'ADDICTO:0000752', 'ADDICTO:0000941', 'ADDICTO:0000410', 'MF:0000016', 'BCIO:037000', 'BCIO:037000', 'BCIO:037000', 'ADDICTO:0000370', 'ADDICTO:0000773', 'ADDICTO:0000405', 'ADDICTO:0000788']
     # print("ontology_id_list is now: ", ontology_id_list)
         # print("get_descendents is: ", should_get_descendents)
     #todo: trying filtering by ontology_id_list before merge - works
@@ -71,12 +88,12 @@ def hv_generator(ontology_id_input, should_get_descendents):
     nodes = hv.Dataset(pd.DataFrame(node_info), 'index')
     nodes.data.head()
 
-    chord = hv.Chord((links, nodes)).select(value=(0, None)) #todo: was value=5 - changed to 0 now it works for more? 
+    chord = hv.Chord((links, nodes)).select(value=(5, None)) #todo: was value=5 - changed to 0 now it works for more? 
 
     chord.opts(
         opts.Chord(cmap='Category20', edge_cmap='Category20', edge_color=dim('source').str(),
                 labels='name', node_color=dim('index').str()))
-
+    print("The time difference is :", timer() - start_time)
     #error message html if no chord plot to show:
     if dcp.empty:
         print('empty dataframe, should create an error message chordout.html here')
